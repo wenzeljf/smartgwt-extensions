c2142f221fdf000d9848f3a33269eea34db33a53
nikolas.lotz <nikolas.lotz@66a99cc0-ca4d-11dd-87b6-9d1a99559f42>
2009-03-24 22:15:41 +0000

    Added GwtRpcDataSource and other changes

---
+++ src/test/java/com/smartgwt/extensions/client/gwtrpcds/test/SimpleGwtRPCDSTestConfig.java
@@ -0,0 +1,13 @@
+package com.smartgwt.extensions.client.gwtrpcds.test;
+
+import java.util.Date;
+
+public interface SimpleGwtRPCDSTestConfig {
+	
+	public static final Integer TEST_RECORD_ID = 1;
+
+	public static final String TEST_RECORD_NAME = "Dummy";
+
+	public static final Date TEST_RECORD_DATE = new Date();
+
+}
