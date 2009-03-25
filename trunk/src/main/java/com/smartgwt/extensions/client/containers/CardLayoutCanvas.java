13c8e570f7b61dc6b7ba4710027369a9ea1cd93c
farrukh.najm <farrukh.najm@66a99cc0-ca4d-11dd-87b6-9d1a99559f42>
2009-03-25 12:56:23 +0000

    Added CardLayoutCanvas.

---
+++ trunk/src/main/java/com/smartgwt/extensions/client/containers/CardLayoutCanvas.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartgwt.extensions.client.containers;
+
+import com.smartgwt.client.widgets.Canvas;
+import java.util.HashMap;
+
+/**
+ * A Canvas that manages its children similar to Swing CardLayout.
+ * It treats each component in the container as a card. Only one card is visible at a time, and the container acts as a stack of cards. The first component added to a CardLayout object is the visible component when the container is first displayed.
+ *
+ * @author farrukh@wellfleetsoftware.com
+ */
+public class CardLayoutCanvas extends Canvas {
+
+    HashMap<Object, Canvas> cards = null;
+
+    public CardLayoutCanvas() {
+        cards = new HashMap<Object, Canvas>();
+    }
+
+    public void addCard(Object key, Canvas card) {
+        card.setWidth("100%");
+        card.setHeight("100%");
+        card.setPageLeft(0);
+        card.setPageTop(0);
+        this.addChild(card);
+        cards.put(key, card);
+    }
+
+    public void showCard(Object key) {
+        for (Object _key : cards.keySet()) {
+            Canvas c = cards.get(_key);
+            if (key == _key) {
+                c.show();
+            } else {
+                c.hide();
+            }
+        }
+    }
+}
