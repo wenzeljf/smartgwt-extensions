32b3e324927be30f6105ae7c94e569475d70bc08
anthony.yuan <anthony.yuan@66a99cc0-ca4d-11dd-87b6-9d1a99559f42>
2009-05-26 15:34:15 +0000

    FileUploader: make it working first. will refactor it a bit later.

--- src/main/java/com/smartgwt/extensions/client/fileuploader/FileRecord.java
+++ src/main/java/com/smartgwt/extensions/client/fileuploader/FileRecord.java
@@ -1,52 +1,55 @@
 package com.smartgwt.extensions.client.fileuploader;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
 
-public class FileRecord extends ListGridRecord {
-	public static final String RD_FIELD_FILE_ID = "fileId";
-	public static final String RD_FIELD_FILE_NAME = "fileName";
-	public static final String RD_FIELD_FOLDER_ID = "folderId";
-	public static final String RD_FIELD_STATUS_CODE = "statusCode";
-	public static final String RD_FIELD_NOTE = "note";
+public class FileRecord extends ListGridRecord implements FileUploaderConstants {
+
+    public FileRecord() {
+	}
+
+	public FileRecord(JavaScriptObject jsObj) {
+		super(jsObj);
+	}
 
 	public String getFileId() {
-		return getAttribute(RD_FIELD_FILE_ID);
+		return getAttribute(RECORD_FIELD_FILE_ID);
 	}
 
 	public void setFileId(String fileId) {
-		setAttribute(RD_FIELD_FILE_ID, fileId);
+		setAttribute(RECORD_FIELD_FILE_ID, fileId);
 	}
 
 	public String getFileName() {
-		return getAttribute(RD_FIELD_FILE_NAME);
+		return getAttribute(RECORD_FIELD_FILE_NAME);
 	}
 
 	public void setFileName(String fileName) {
-		setAttribute(RD_FIELD_FILE_NAME, fileName);
+		setAttribute(RECORD_FIELD_FILE_NAME, fileName);
 	}
 
-	public String getStatusCode() {
-		return getAttribute(RD_FIELD_STATUS_CODE);
+	public FileState getFileState() {
+		return (FileState) getAttributeAsObject(RECORD_FIELD_FILE_STATE);
 	}
 
-	public void setStatusCode(String statusCode) {
-		setAttribute(RD_FIELD_STATUS_CODE, statusCode);
-	}
-
-	public void setStatusCode(FileState statusCode) {
-		if (statusCode != null) {
-			setAttribute(RD_FIELD_STATUS_CODE, statusCode.toString());
-		} else {
-			setAttribute(RD_FIELD_STATUS_CODE, statusCode);
-		}
+	public void setFileState(FileState fileState) {
+		setAttribute(RECORD_FIELD_FILE_STATE, fileState);
 	}
 
 	public String getNote() {
-		return getAttribute(RD_FIELD_NOTE);
+		return getAttribute(RECORD_FIELD_NOTE);
 	}
 
 	public void setNote(String note) {
-		setAttribute(RD_FIELD_NOTE, note);
+		setAttribute(RECORD_FIELD_NOTE, note);
+	}
+
+	public FileUploadInput getFileUploadInput() {
+		return (FileUploadInput) getAttributeAsObject(RECORD_FIELD_FILE_INPUT);
+	}
+
+	public void setFileUploadInput(FileUploadInput input) {
+		setAttribute(RECORD_FIELD_FILE_INPUT, input);
 	}
 
 }
---
+++ src/main/java/com/smartgwt/extensions/client/fileuploader/SimpleButton.java
@@ -0,0 +1,15 @@
+package com.smartgwt.extensions.client.fileuploader;
+
+import com.smartgwt.client.widgets.Button;
+
+public class SimpleButton extends Button {
+	
+	public SimpleButton(String title, String icon) {
+		super(title);
+
+		setIcon(icon);
+		setShowRollOver(false);
+		setShowDownIcon(false);
+	}
+
+}
